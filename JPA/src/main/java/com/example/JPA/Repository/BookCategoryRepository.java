4a6d325319de373a5934abc54cb7f57d83327333
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2023-07-26 21:15:28 +0530

    Delete JPA directory

--- JPA/src/main/java/com/example/JPA/Repository/BookCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.JPA.Repository;
-
-import com.example.JPA.Models.BookCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookCategoryRepository extends JpaRepository<BookCategory,Integer> {
-}
